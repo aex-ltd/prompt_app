f951f5be3b5f326b4f005c402a93b48839a2b6a8
cletus <cletushunsu@gmail.com>
2024-09-14 02:28:15 -0700

    created custom user admin class

---
+++ authentication/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin 
+
+from .models import CustomUser 
+
+# customuser admin 
+class CustomUserAdmin(UserAdmin):
+
+    model = CustomUser
+
+    list_display = ['email', 'is_active', 'is_staff', 'is_superuser', 'last_login']
+    list_display_links = ['email']
+    list_filter = ['is_active', 'is_staff', 'is_superuser', 'last_login']
+    fieldsets = [
+        ('Basic Info', {'fields': ('email','passowrd')}),
+        ('Permissions', {'fields':('is_active', 'is_staff','is_superuser', 'groups', 'permissions')}),
+        ('Dates', {'fields': ('last_login',)})
+    ]
+    ordering = ('username', 'email')
+    search_fields = ['email']
+
+admin.site.register(CustomUser, CustomUserAdmin)
+    
