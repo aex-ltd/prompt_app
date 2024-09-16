1a70e7188b243d66cf672392bf9fa64124000ade
cletus <cletushunsu@gmail.com>
2024-09-16 02:29:10 -0700

    created TExtPrompt and FilePrompt model

--- user_prompt/models.py
+++ user_prompt/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+# text prompt model 
+class TextPrompt(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='textprompt_user')
+    context = models.TextField()
+    role = models.TextField()
+    goal = models.TextField()
+    restrictions = models.TextField()
+    audience = models.TextField()
+    writing_style = models.TextField()
+    tone = models.TextField()
+    keywords = models.TextField()
+    examples = models.TextField()
+    file =  models.FileField(upload_to='files/', blank=True, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.email 
+    
+    class Meta:
+        ordering = ['-date']
+
+
+# file prompt model 
+class FilePrompt(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='fileprompt_user')
+    file = models.FileField(upload_to='files/', blank=True, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.email 
+    
+    class Meta:
+        ordering = ['-date']
+
+    
+    
