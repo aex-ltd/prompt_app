bcaf1d87c063935deccf7223cbee8f0d793e76de
cletus <cletushunsu@gmail.com>
2024-09-17 02:46:43 -0700

    configured media and static files settings

--- prompt/settings.py
+++ prompt/settings.py
@@ -109,6 +109,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),
+                    os.path.join(BASE_DIR, 'authentication/static'),
+                    os.path.join(BASE_DIR, 'user_prompt/static'),
+                    ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# media settings 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
