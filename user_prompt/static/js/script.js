c279933af88c376b5d94951b6eaf17d16889c2bf
cletus <cletushunsu@gmail.com>
2024-09-18 07:47:05 -0700

    included script to hide django message after 2.5 sec

--- user_prompt/static/js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('textPromptForm');
-    const loading = document.getElementById('loading');
-
-    form.addEventListener('submit', () => {
-        // Display loading box
-        loading.classList.remove('hidden');
-    });
-});
