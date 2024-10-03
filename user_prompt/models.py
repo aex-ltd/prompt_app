f046ad2a9bdf64782abbd83464cef7e4025bf9da
cletus <cletushunsu@gmail.com>
2024-10-03 10:53:27 -0700

    created hint model

--- user_prompt/models.py
+++ user_prompt/models.py
@@ -26,22 +26,7 @@ class TextPrompt(models.Model):
     
     class Meta:
         ordering = ['-date']
-
-
-# file prompt model 
-# class FilePrompt(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='fileprompt_user')
-#     title = models.CharField(max_length=60, blank=True, null=True)
-#     file = models.FileField(upload_to='files/', blank=True, null=True)
-#     date = models.DateTimeField(auto_now_add=True)
-
-#     def __str__(self):
-#         return self.user.email 
-    
-#     class Meta:
-#         ordering = ['-date']
-
-    
+ 
 # hint class 
 class Hint(models.Model):
     context = models.CharField(max_length=300, null=True, blank=True)
@@ -56,5 +41,6 @@ class Hint(models.Model):
     examples = models.CharField(max_length=300, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
     
 
