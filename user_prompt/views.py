7e1d38fe8df8d3a45e16bd2f5bb56fa49966877e
cletus <cletushunsu@gmail.com>
2024-10-03 10:53:54 -0700

    created prompt fields views

--- user_prompt/views.py
+++ user_prompt/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-# from django.core.mail import send_mail
-# from django.conf import settings 
 
-# from .services import ask_gpt, ask_gemini
-from .forms import TextPromptForm
+from .forms import ( TextPromptForm, HintForm, Context, Role,
+                    Goal, Restrictions, Audience, FormatResult,
+                      WritingStyle, Tone, Keywords, Examples
+)
+
 from .models import TextPrompt, Hint
 
 User = get_user_model()
@@ -57,3 +58,170 @@ def prompt_view(request):
     # GET request, render empty form
     form = TextPromptForm()
     return render(request, 'chat.html', {'form': form})
+
+
+# prompt context view 
+def prompt_context(request):
+    hint = Hint.objects.all()
+    if request.method == 'POST':
+        form = Context(request.POST)
+
+        if form.is_valid():
+            pass 
+        
+        else:
+            messages.error(request, "Invalid data format. Please try again")
+            return render(request, 'context.html', {'form':form, 'hint': hint.context})
+    
+    form = Context()
+    return render(request, 'context.html', {'form':form, 'hint': hint.context})
+
+
+# prompt role view 
+def prompt_role(request):
+    hint = Hint.objects.all()
+    if request.method == 'POST':
+        form = Role(request.POST)
+
+        if form.is_valid():
+            pass 
+        
+        else:
+            messages.error(request, "Invalid data format. Please try again")
+            return render(request, 'role.html', {'form':form, 'hint': hint.role})
+    
+    form = Role()
+    return render(request, 'role.html', {'form':form, 'hint': hint.role})
+
+
+#prompt goal view 
+def prompt_goal(request):
+    hint = Hint.objects.all()
+    if request.method == 'POST':
+        form = Goal(request.POST)
+
+        if form.is_valid():
+            pass 
+        
+        else:
+            messages.error(request, "Invalid data format. Please try again")
+            return render(request, 'goal.html', {'form':form, 'hint': hint.goal})
+    
+    form = Goal()
+    return render(request, 'goal.html', {'form':form, 'hint': hint.goal})
+
+
+# prompt restrictions view 
+def prompt_restrictions(request):
+    hint = Hint.objects.all()
+    if request.method == 'POST':
+        form = Restrictions(request.POST)
+
+        if form.is_valid():
+            pass 
+        
+        else:
+            messages.error(request, "Invalid data format. Please try again")
+            return render(request, 'restrictions.html', {'form':form, 'hint': hint.restrictions})
+    
+    form = Restrictions()
+    return render(request, 'restrictions.html', {'form':form, 'hint': hint.restrictions})
+
+# prompt audience view 
+def prompt_audience(request):
+    hint = Hint.objects.all()
+    if request.method == 'POST':
+        form = Audience(request.POST)
+
+        if form.is_valid():
+            pass 
+        
+        else:
+            messages.error(request, "Invalid data format. Please try again")
+            return render(request, 'audience.html', {'form':form, 'hint': hint.audience})
+    
+    form = Audience()
+    return render(request, 'audience.html', {'form':form, 'hint': hint.audience})
+
+
+# prompt format view 
+def prompt_format(request):
+    hint = Hint.objects.all()
+    if request.method == 'POST':
+        form = FormatResult(request.POST)
+
+        if form.is_valid():
+            pass 
+        
+        else:
+            messages.error(request, "Invalid data format. Please try again")
+            return render(request, 'format_result.html', {'form':form, 'hint': hint.format_result})
+    
+    form = FormatResult()
+    return render(request, 'format_result.html', {'form':form, 'hint': hint.format_result})
+
+# prompt Writing style view 
+def prompt_writing_style(request):
+    hint = Hint.objects.all()
+    if request.method == 'POST':
+        form = WritingStyle(request.POST)
+
+        if form.is_valid():
+            pass 
+        
+        else:
+            messages.error(request, "Invalid data format. Please try again")
+            return render(request, 'writing_style.html', {'form':form, 'hint': hint.writing_style})
+    
+    form = WritingStyle()
+    return render(request, 'writing_style.html', {'form':form, 'hint': hint.writing_style})
+
+
+# prompt tone view 
+def prompt_tone(request):
+    hint = Hint.objects.all()
+    if request.method == 'POST':
+        form = Tone(request.POST)
+
+        if form.is_valid():
+            pass 
+        
+        else:
+            messages.error(request, "Invalid data format. Please try again")
+            return render(request, 'tone.html', {'form':form, 'hint': hint.tone})
+    
+    form = Tone()
+    return render(request, 'tone.html', {'form':form, 'hint': hint.tone})
+
+# prompt keywords view 
+def prompt_keywords(request):
+    hint = Hint.objects.all()
+    if request.method == 'POST':
+        form = Keywords(request.POST)
+
+        if form.is_valid():
+            pass 
+        
+        else:
+            messages.error(request, "Invalid data format. Please try again")
+            return render(request, 'keywords.html', {'form':form, 'hint': hint.keywords})
+    
+    form = Keywords()
+    return render(request, 'keywords.html', {'form':form, 'hint': hint.keywords})
+
+
+# prompt example view 
+def prompt_example(request):
+    hint = Hint.objects.all()
+    if request.method == 'POST':
+        form = Examples(request.POST)
+
+        if form.is_valid():
+            pass 
+        
+        else:
+            messages.error(request, "Invalid data format. Please try again")
+            return render(request, 'examples.html', {'form':form, 'hint': hint.examples})
+    
+    form = Examples()
+    return render(request, 'examples.html', {'form':form, 'hint': hint.examples})
