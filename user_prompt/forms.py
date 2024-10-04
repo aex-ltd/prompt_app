dd045227a9993fcd2740df1ef6d02175a8dc2e84
cletus <cletushunsu@gmail.com>
2024-10-03 23:42:27 -0700

    created forms for individual prompt fields

--- user_prompt/forms.py
+++ user_prompt/forms.py
@@ -9,45 +9,50 @@ class TextPromptForm(forms.ModelForm):
         model = TextPrompt
         fields = ['title', 'context', 'role', 'goal', 'restrictions', 'audience','format_result', 'writing_style', 'tone', 'keywords', 'examples']
 
+
+# prompt title form 
+class Title(forms.Form):
+    title = forms.CharField(required=True)
+    
 # prompt context form 
-class Context(forms.ModelForm):
-    context = forms.CharField(required=False)
+class Context(forms.Form):
+    context = forms.CharField(widget=forms.Textarea(attrs={"rows":"5"}), required=False)
 
 # prompt role form 
-class Role(forms.ModelForm):
-    role = forms.CharField(required=False)
+class Role(forms.Form):
+    role = forms.CharField(widget=forms.Textarea(attrs={"rows":"5"}), required=False)
 
 # prompt goal form 
-class Goal(forms.ModelForm):
-    goal = forms.CharField(required=False)
+class Goal(forms.Form):
+    goal = forms.CharField(widget=forms.Textarea(attrs={"rows":"5"}), required=False)
 
 # prompt restrictions form 
-class Restrictions(forms.ModelForm):
-    restrictions = forms.CharField(required=False)
+class Restrictions(forms.Form):
+    restrictions = forms.CharField(widget=forms.Textarea(attrs={"rows":"5"}), required=False)
 
 # prompt audience form 
-class Audience(forms.ModelForm):
-    audience = forms.CharField(required=False)
+class Audience(forms.Form):
+    audience = forms.CharField(widget=forms.Textarea(attrs={"rows":"5"}), required=False)
 
 # prompt format form 
-class FormatResult(forms.ModelForm):
-    format_result = forms.CharField(required=False)
+class FormatResult(forms.Form):
+    format_result = forms.CharField(widget=forms.Textarea(attrs={"rows":"5"}), required=False)
 
 # prompt writing style form 
-class WritingStyle(forms.ModelForm):
-    writing_style = forms.CharField(required=False)
+class WritingStyle(forms.Form):
+    writing_style = forms.CharField(widget=forms.Textarea(attrs={"rows":"5"}), required=False)
 
 # prompt tone form 
-class Tone(forms.ModelForm):
-    tone = forms.CharField(required=False)
+class Tone(forms.Form):
+    tone = forms.CharField(widget=forms.Textarea(attrs={"rows":"5"}), required=False)
 
 # prompt keywords form 
-class Keywords(forms.ModelForm):
-    keywords = forms.CharField(required=False)
+class Keywords(forms.Form):
+    keywords = forms.CharField(widget=forms.Textarea(attrs={"rows":"5"}), required=False)
 
 # prompt example form 
-class Examples(forms.ModelForm):
-    example = forms.CharField(required=False)
+class Examples(forms.Form):
+    example = forms.CharField(widget=forms.Textarea(attrs={"rows":"5"}), required=False)
 
 
 # hint form 
