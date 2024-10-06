afd292da699a9e52eb7298a7b5549900ddca36aa
cletus <cletushunsu@gmail.com>
2024-10-06 00:27:01 -0700

    Created url path for prompt_example view

--- user_prompt/urls.py
+++ user_prompt/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.static import static
 
 from .views import (
     dashboard, prompt_view, prompt_context, prompt_audience,
-    prompt_example, prompt_format, prompt_goal, prompt_keywords,prompt_title,
+    prompt_examples, prompt_format, prompt_goal, prompt_keywords,prompt_title,
     prompt_restrictions, prompt_role, prompt_tone, prompt_writing_style
 )
 
@@ -13,7 +13,7 @@ urlpatterns = [
     path('prompt_title/', prompt_title, name='prompt_title'),
     path('prompt_context/', prompt_context, name='prompt_context'),
     path('prompt_audience/', prompt_audience, name='prompt_audience'),
-    path('prompt_example/', prompt_example, name="prompt_example"),
+    path('prompt_example/', prompt_examples, name="prompt_examples"),
     path('prompt_format/', prompt_format, name='prompt_format'),
     path('prompt_goal/', prompt_goal, name='prompt_goal'),
     path('prompt_keywords/', prompt_keywords, name='prompt_keywords'),
